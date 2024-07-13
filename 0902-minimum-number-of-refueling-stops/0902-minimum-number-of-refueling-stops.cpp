6cf03d110aa7ef34be4e1e6c26d4002cb54a9ebe
Kunal Saini <66879424+sainiks@users.noreply.github.com>
2024-07-13 20:32:57 +0530

    Time: 11 ms (98.44%), Space: 20.3 MB (17.38%) - LeetHub

--- 0902-minimum-number-of-refueling-stops/0902-minimum-number-of-refueling-stops.cpp
+++ 0902-minimum-number-of-refueling-stops/0902-minimum-number-of-refueling-stops.cpp
@@ -3,19 +3,19 @@ public:
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
         sort(stations.begin(),stations.end());
         int n=stations.size();
-        priority_queue<int>acp;
+        priority_queue<int>acpp;
         int nextpump=0;
         int curpos=startFuel;
         int stops=0;
         while(true){
             while(nextpump < n and stations[nextpump][0] <=curpos){
-                acp.push(stations[nextpump][1]);
+                acpp.push(stations[nextpump][1]);
                 nextpump++;
             }
             if(curpos>=target)return stops ;
-            if(acp.empty())break;
-            curpos+=acp.top();
-            acp.pop();
+            if(acpp.empty())break;
+            curpos+=acpp.top();
+            acpp.pop();
             stops++;
         }
         return -1;
