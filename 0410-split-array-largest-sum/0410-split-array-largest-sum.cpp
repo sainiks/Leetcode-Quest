753cf099f7896a31b3b5189973d25adc9891f35c
Kunal Saini <66879424+sainiks@users.noreply.github.com>
2024-07-19 15:09:17 +0530

    Time: 237 ms (5.03%), Space: 10.6 MB (5.39%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    int splitArray(vector<int>& nums, int m) {
-        int n = nums.size();
-        vector<int> prefixSum(n + 1, 0);
-        for (int i = 0; i < n; ++i) {
-            prefixSum[i + 1] = prefixSum[i] + nums[i];
+    int splitArray(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>pref(n+1,0);
+        for(int i=1; i<=n;i++){
+            pref[i]=pref[i-1]+nums[i-1];
         }
-        
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
-        dp[0][0] = 0;
-
-        for (int k = 1; k <= m; ++k) {
-            for (int i = 1; i <= n; ++i) {
-                for (int j = 0; j < i; ++j) {
-                    dp[k][i] = min(dp[k][i], max(dp[k - 1][j], prefixSum[i] - prefixSum[j]));
+        vector<vector<int>>dp(n+1,vector<int>(k+1,1e9));
+        for(int i=0;i<=k;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=k;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1){
+                    dp[j][i]=pref[j]-pref[0];
+                }else{
+                    for(int l=j;l>0;l--){
+                        int sum=pref[j]-pref[l-1];
+                        dp[j][i]=min(dp[j][i],max(dp[l-1][i-1],sum));
+                    }
                 }
             }
         }
-
-        return dp[m][n];
+        return dp[n][k];
     }
 };
